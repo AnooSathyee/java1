795dbc2f11fa71a6e33af60e8fa4e45246b95243
Anu <anoosathyee@gmail.com>
2021-03-10 14:57:17 -0600

    added switch-case

--- src/com/anu/ControlFlow.java
+++ src/com/anu/ControlFlow.java
@@ -12,10 +12,30 @@ public class ControlFlow {
             System.out.println("I'm not hungry");
         }
         int hungerRating = 7;
-        if(hungerRating > 5){
+        if (hungerRating > 5) {
             System.out.println("Not Hungry");
         } else {
             System.out.println("I'm starving");
         }
+
+        // switch-case statements
+        int monthN = 3;
+        String month = null;
+        switch (monthN) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            default:
+                month = "unknown Month";
+                break;
+        }
+        System.out.println(month);
+
     }
 }
