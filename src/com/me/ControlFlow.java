517a5e247046eabefb122f7b67548db30b47b164
Anu <anoosathyee@gmail.com>
2021-03-10 14:39:03 -0600

    added few examples

--- src/com/me/ControlFlow.java
+++ src/com/me/ControlFlow.java
@@ -4,5 +4,19 @@ public class ControlFlow {
 
     public static void main(String[] args) {
 
+        boolean hungry = true;
+
+        if (hungry) {
+            System.out.println("I'm starving");
+        } else {
+            System.out.println("I'm not hungry");
+        }
+
+        int hungerRating = 7;
+        if(hungerRating > 5){
+            System.out.println("Not Hungry");
+        } else {
+            System.out.println("I'm starving");
+        }
     }
 }
