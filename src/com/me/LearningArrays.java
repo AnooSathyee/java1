d94a3b06b5a35c1c9624d01a79f5918d2ab549a9
Anu <anoosathyee@gmail.com>
2021-03-10 14:22:01 -0600

    Initial commit

---
+++ src/com/me/LearningArrays.java
@@ -0,0 +1,13 @@
+package com.me;
+
+
+public class LearningArrays {
+    public static void main(String[] args) {
+        int[] values = new int[3];
+        for(int i : values){
+            System.out.println(values[i]);
+        }
+        //System.out.println(values[2]);
+    }
+
+}
