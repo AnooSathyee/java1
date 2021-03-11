4b5c8350bfdb613737814ad11ae9483f34345319
Anu <anoosathyee@gmail.com>
2021-03-10 18:36:49 -0600

    created two packages

---
+++ src/com/anu/masterclass/DataTypes.java
@@ -0,0 +1,12 @@
+package com.anu.masterclass;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int firstNumber = (10 + 5) + (2 * 10);
+        int secondNumber = 12;
+        int thirdNumber = firstNumber * 2;
+        int total = firstNumber + secondNumber + thirdNumber;
+        int lastOne = 1000-total;
+        System.out.println(lastOne);
+    }
+}
---
+++ src/com/anu/masterclass/Hello.java
@@ -0,0 +1,8 @@
+package com.anu.masterclass;
+
+public class Hello {
+
+    public static void main(String[] args) {
+    System.out.println("Hello Anu!");
+    }
+}
