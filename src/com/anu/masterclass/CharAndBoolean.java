b5fcd0033e49d82ad269921effe2c055bfbe6ed3
Anu <anoosathyee@gmail.com>
2021-03-11 11:32:24 -0600

    char and Boolean Data Types

--- src/com/anu/masterclass/CharAndBoolean.java
+++ src/com/anu/masterclass/CharAndBoolean.java
@@ -1,2 +1,12 @@
-package com.anu.masterclass;public class CharAndBoolean {
+package com.anu.masterclass;
+
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        //©
+        System.out.println(myChar);
+    }
+
+
 }
