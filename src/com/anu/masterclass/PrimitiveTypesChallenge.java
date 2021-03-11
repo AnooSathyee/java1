b4b394ac2313767082ecdb1d74b900ace69fb301
Anu <anoosathyee@gmail.com>
2021-03-10 21:27:10 -0600

    created Datatypes class

---
+++ src/com/anu/masterclass/PrimitiveTypesChallenge.java
@@ -0,0 +1,14 @@
+package com.anu.masterclass;
+
+public class PrimitiveTypesChallenge {
+
+    public static void main(String[] args) {
+
+        byte b = 120;
+        short s = 3000;
+        int i = 10000;
+        long l = 50000 + (10 * b) + (10 * s);
+        System.out.println(l);
+
+    }
+}
