173e39c14f5e90609233c9e25dfc600d27cbf9ed
Anu <anoosathyee@gmail.com>
2021-03-16 11:57:23 -0500

    Done few method challenges

---
+++ src/com/anu/masterclass/MethodChallenge.java
@@ -0,0 +1,28 @@
+package com.anu.masterclass;
+
+public class MethodChallenge {
+
+    public static void displayHighScorePosition(String playersName, int highScorePosition){
+        System.out.println(playersName + " managed to get into a position " + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if(score >= 1000){
+        return 1;
+        }else if(score >= 500 && score < 1000){
+            return 2;
+        } else if(score >= 100 && score < 500){
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Anu", highScorePosition);
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Anya", highScorePosition);
+
+    }
+}
---
+++ src/com/anu/masterclass/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+package com.anu.masterclass;
+
+public class PositiveNegativeZero {
+
+    public static void checkNumber(int num) {
+        if (num > 0) {
+            System.out.println("+ve");
+        } else if (num < 0) {
+            System.out.println("-ve");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
