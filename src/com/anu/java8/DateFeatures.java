cc155eec5d99ffc1f1aa77c0545017be57aa220e
Anu <anoosathyee@gmail.com>
2021-04-03 06:00:32 -0500

    added time features

--- src/com/anu/java8/DateFeatures.java
+++ src/com/anu/java8/DateFeatures.java
@@ -1,9 +1,11 @@
 package com.anu.java8;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DateFeatures {
     public static void main(String[] args) {
+        // LocalDate is a predefined classes
         LocalDate ld = LocalDate.now();
         System.out.println(ld);
         System.out.println("year >> " + ld.getYear());
@@ -13,5 +15,14 @@ public class DateFeatures {
         System.out.println("Today's day >> " + ld.getDayOfMonth());
         System.out.println("Future Day >>> " + ld.plusDays(2));
         System.out.println("Past Date >> " + ld.minusDays(1));
+
+        // getting only time
+        LocalTime lt = LocalTime.now();
+        System.out.println("Current Local Time >> " + lt);
+        System.out.println("Hour >>> " + lt.getHour());
+        System.out.println("Minute >>> " + lt.getMinute());
+        System.out.println("Second >>> " + lt.getSecond());
+        System.out.println("Nano Second >>> " + lt.getNano());
+
     }
 }
