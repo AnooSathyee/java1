9030c25967e6c445bc60abf9571f682a52f5a721
Anu <anoosathyee@gmail.com>
2021-04-03 06:15:13 -0500

    added few more challenges

--- src/com/anu/java8/DateFeatures.java
+++ src/com/anu/java8/DateFeatures.java
@@ -1,7 +1,9 @@
 package com.anu.java8;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateFeatures {
     public static void main(String[] args) {
@@ -23,6 +25,17 @@ public class DateFeatures {
         System.out.println("Minute >>> " + lt.getMinute());
         System.out.println("Second >>> " + lt.getSecond());
         System.out.println("Nano Second >>> " + lt.getNano());
+
+        //LocalDateTime API
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+        // DateTimeFormatter
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM DD YYYY");
+        String s = "Jun 21 1979";
+        //LocalDate lld = LocalDate.parse(s,dtf);
+        System.out.println();
+        //System.out.println(lld);
 
     }
 }
---
+++ src/com/anu/java8/LocalDateTimeJava8.java
@@ -0,0 +1,18 @@
+package com.anu.java8;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeJava8 {
+
+    public static void main(String[] args) {
+
+        // DateTimeFormatter
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM DD YYYY");
+//        String s = "Jun 21 1979";
+//        LocalDate ld = LocalDate.parse(s,dtf);
+//        System.out.println();
+//        System.out.println(ld);
+
+    }
+}
