154996f955110e09c2ebf4dd50ed8b5042c1f12a
Anu <anoosathyee@gmail.com>
2021-04-06 17:10:08 -0500

    sorting ArrayList

---
+++ src/com/anu/w3_arraylist/Exercise_8.java
@@ -0,0 +1,24 @@
+package com.anu.w3_arraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Exercise_8 {
+
+    public static void main(String[] args) {
+
+        // Sort the ArrayList
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Apple");
+        list.add("Orange");
+        list.add("Pineapple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Grapes");
+        System.out.println("List before sorting >>> ");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println("List after sorting >>> ");
+        System.out.println(list);
+    }
+}
