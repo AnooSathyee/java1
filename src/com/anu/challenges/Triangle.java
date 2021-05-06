f57125fe1deb898840bd09b4c8b595c3aa8086b1
Anu <anoosathyee@gmail.com>
2021-05-06 15:44:32 -0500

    solved Triangle Challenge

--- src/com/anu/challenges/Triangle.java
+++ src/com/anu/challenges/Triangle.java
@@ -2,5 +2,29 @@ package com.anu.challenges;
 
 public class Triangle {
 
+  int a;
+  int b;
+  int c;
 
+  public double area(){
+      double p = (a+b+c)/2;
+      return Math.pow(p*(p-a) * (p-b) * (p-c),.5);
+  }
+
+  public double perimeter(){
+      return (a+b+c)/2;
+  }
+
+}
+
+class Driver{
+
+    public static void main(String[] args) {
+        Triangle t = new Triangle();
+        t.a = 3;
+        t.b = 4;
+        t.c = 5;
+        System.out.println("Area of a triangle >>> " + t.area());
+        System.out.println("Perimeter of a triangle >>> " + t.perimeter());
+    }
 }
